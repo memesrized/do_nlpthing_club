1808a9fd7fd2083193417e579aa4357e0b221160
Vasilii Salikov <memesrized@gmail.com>
2021-08-29 16:23:45 +0300

    Add init scripts

---
+++ dnc/__init__.py
@@ -0,0 +1,5 @@
+from dnc.lib_utils import PathLoader
+
+PROJECT_PATHS = PathLoader()
+
+RANDOM_STATE = 1337
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dnc",
+    packages=find_packages(),
+    version="0.0.1",
+    description="We do nlpthing",
+    author="Vasilii Salikov",
+    license="LICENSE",
+)

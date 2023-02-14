8858bd9d0280be38650b6a0cdb25bafcc09878db
Vasilii Salikov <memesrized@gmail.com>
2023-02-14 23:41:57 +0200

    Improve tokenizer and dataset

--- dnc/distillation/code/dataset.py
+++ dnc/distillation/code/dataset.py
@@ -1,22 +1,22 @@
-from itertools import chain
-
 import pandas as pd
 import torch
-from segtok.tokenizer import word_tokenizer
 from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 from tqdm import tqdm
 
 
 class EmbDataset(Dataset):
     def __init__(self, data_path, tokenizer, size=None, text_col="text", target_col="sentiment", mean=True):
         self.X, self.y = zip(*pd.read_csv(data_path)[[text_col, target_col]][:size].to_numpy())
-        
         self.tokenizer = tokenizer
+        self.X = [self.tokenizer(x) for x in tqdm(self.X, desc="tokenizer")]
 
-        self.X = [self.tokenizer(x) for x in tqdm(self.X)]
+        non_empty_mask = {i for i,x in enumerate(self.X) if x.nelement()}
+        self.X = [x for i,x in enumerate(self.X) if i in non_empty_mask]
+        self.y = [y for i,y in enumerate(self.y) if i in non_empty_mask]
+        
         if mean:
-            self.X = [x.mean(0) for x in tqdm(self.X)]
+            self.X = [x.mean(0) for x in tqdm(self.X, desc="mean")]
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
@@ -24,6 +24,7 @@ class EmbDataset(Dataset):
     def __len__(self):
         return len(self.X)
 
+    
 
 class Collator:
     def __init__(self, pad_value=0):
--- dnc/distillation/code/tokenizer.py
+++ dnc/distillation/code/tokenizer.py
@@ -1,3 +1,5 @@
+import re
+
 from razdel import tokenize as razdel_tokenizer
 
 from dnc.distillation.code.embeddings import CustomEmbeddings
@@ -9,16 +11,51 @@ logger = get_logger(__name__)
 # TODO: rewrite with vocab when emb=None
 # TODO: rewrite to use it in collator
 class Tokenizer:
-    def __init__(self, emb, unk_token="<unk>", pad_token="<pad>"):
+    def __init__(
+        self,
+        emb,
+        # unk_token="<unk>",
+        # pad_token="<pad>",
+        stopwords=None,
+        isalpha=True,
+        lower=True,
+    ):
         if not emb:
             logger.warning("NO EMBEDDINGS!")
         self.emb = self._load_emb(emb)
-    
+        if stopwords:
+            with open(stopwords, encoding="utf-8") as file:
+                stopwords = {x.strip() for x in file.readlines()}
+                self.stopwords = stopwords
+        else:
+            self.stopwords = False
+        self.isalpha = isalpha
+        self.isalpha_re = re.compile(
+            "[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]", flags=re.IGNORECASE
+        )
+        self.lower = lower
+
     def tokenize(self, text):
         return [_.text for _ in razdel_tokenizer(text)]
-    
+
     def __call__(self, text):
-        return self.emb([_.text for _ in razdel_tokenizer(text)])
-    
+        if self.lower:
+            text = text.lower()
+        text = self.tokenize(text)
+        if self.stopwords:
+            text = self.remove_stopwords(text)
+        if self.isalpha:
+            text = self.remove_non_alpha(text)
+        return self.emb(text)
+
+    def embed(self, seq):
+        return self.emb(seq)
+
+    def remove_stopwords(self, seq):
+        return [t for t in seq if t.lower() not in self.stopwords]
+
+    def remove_non_alpha(self, seq):
+        return [t for t in seq if self.isalpha_re.search(t)]
+
     def _load_emb(self, path):
         return CustomEmbeddings(path)
